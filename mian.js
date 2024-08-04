6af4a2a6c9ddaa9ce3a0a51a2aa69c55c18c6629
Jposeph99 <joseph.ahmed65@yahoo.com>
2024-08-04 18:11:57 -0400

    project is done ready to deploy lol

---
+++ mian.js
@@ -0,0 +1,26 @@
+var popUp = document.querySelector(".calculate");
+var popUPContainer = document.querySelector(".video-container");
+var closePopUpBtn = document.querySelector(".close-popup");
+
+var video = document.getElementById('video')
+video.pause()
+
+function openPopUp() {
+  popUPContainer.classList.add("active");
+  video.play()
+  console.log(popUp, popUPContainer);
+}
+
+function closePopUp() {
+  popUPContainer.classList.remove("active");
+  video.pause();
+  video.currentTime = 0;
+}
+
+popUp.addEventListener("click", () => {
+  openPopUp();
+});
+
+closePopUpBtn.addEventListener('click', ()=> {
+    closePopUp()
+})
